5fb138f7da3796f3652f69eb8a7e6fcf38475d15
nicolas fernandez <fernandeznicolas1801@gmail.com>
2024-04-26 18:44:37 -0300

    Filter css changed

--- Movie-App-React-WebApp/movie-webapp/src/pages/MovieSearch/MovieSearch.jsx
+++ Movie-App-React-WebApp/movie-webapp/src/pages/MovieSearch/MovieSearch.jsx
@@ -13,6 +13,8 @@ const MovieSearch = () => {
   const [selectedGenres, setSelectedGenres] = useState([]);
   const [actor, setActor] = useState("");
   const [director, setDirector] = useState("");
+  
+  const [showFilter, setShowFilter] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -28,6 +30,8 @@ const MovieSearch = () => {
     const searchedMovies = await fetchMovies(query);
     setMovies(searchedMovies);
   };
+
+const handleShowFilter = () => setShowFilter(true);
 
 const handleMovieSelect = (movie) => {
   setActiveMovie(movie);
@@ -56,36 +60,45 @@ useEffect(() => {
   return (
     <div className="min-h-screen bg-netflix-dark text-white px-4 py-8">
       <div className="max-w mx-auto">
-        <form onSubmit={handleSearch} className="flex flex-col sm:flex-row gap-4 mb-4">
-          <input
-            type="text"
-            placeholder="Search movies..."
-            value={query}
-            onChange={(e) => setQuery(e.target.value)}
-            className="flex-1 p-2 outline-none rounded-lg focus:ring-2 focus:ring-netflix-red focus:bg-gray-800 bg-gray-700 text-white placeholder-gray-400"
-          />
-          <input
-            type="text"
-            placeholder="Actor"
-            value={actor}
-            onChange={(e) => setActor(e.target.value)}
-            className="flex-1 p-2 outline-none rounded-lg focus:ring-2 focus:ring-netflix-red focus:bg-gray-800 bg-gray-700 text-white placeholder-gray-400"
-          />
-          <input
-            type="text"
-            placeholder="Director"
-            value={director}
-            onChange={(e) => setDirector(e.target.value)}
-            className="flex-1 p-2 outline-none rounded-lg focus:ring-2 focus:ring-netflix-red focus:bg-gray-800 bg-gray-700 text-white placeholder-gray-400"
-          />
-          <button
-            type="submit"
-            className="px-4 py-2 bg-netflix-red hover:bg-red-700 rounded-lg transition duration-300"
-          >
-            Search
-          </button>
-          <GenreFilter genres={genres} selectedGenres={selectedGenres} onGenreChange={handleGenreChange} />
-        </form>
+        <div>
+          <form onSubmit={handleSearch} className="flex flex-col sm:flex-row gap-4 mb-4">
+            <input
+              type="text"
+              placeholder="Search movies..."
+              value={query}
+              onChange={(e) => setQuery(e.target.value)}
+              className="flex-1 p-2 outline-none rounded-lg focus:ring-2 focus:ring-netflix-red focus:bg-gray-800 bg-gray-700 text-white placeholder-gray-400"
+            />
+            <input
+              type="text"
+              placeholder="Actor"
+              value={actor}
+              onChange={(e) => setActor(e.target.value)}
+              className="flex-1 p-2 outline-none rounded-lg focus:ring-2 focus:ring-netflix-red focus:bg-gray-800 bg-gray-700 text-white placeholder-gray-400"
+            />
+            <input
+              type="text"
+              placeholder="Director"
+              value={director}
+              onChange={(e) => setDirector(e.target.value)}
+              className="flex-1 p-2 outline-none rounded-lg focus:ring-2 focus:ring-netflix-red focus:bg-gray-800 bg-gray-700 text-white placeholder-gray-400"
+            />
+            <button
+              type="submit"
+              className="px-4 py-2 bg-netflix-red hover:bg-red-700 rounded-lg transition duration-300"
+            >
+              Search
+            </button>
+            <button
+              className="px-4 py-2 bg-netflix-red hover:bg-red-700 rounded-lg transition duration-300"
+              onClick={handleShowFilter}
+            >
+              Advanced Search
+            </button>
+          </form>
+          {showFilter && <GenreFilter genres={genres} selectedGenres={selectedGenres} onGenreChange={handleGenreChange} onClose={()=>setShowFilter(false)}/>}
+        </div>
+        
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6 mt-2">
           {movies.map((movie) => (
             <div key={movie.id} onClick={() => handleMovieSelect(movie)}>
