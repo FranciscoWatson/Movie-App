2e5b2a9706a713a2a3fe8981505f0020e5507061
FranciscoWatson <fwatson@restaurant365.com>
2024-06-28 18:11:44 -0300

    Update UserProfile

--- Movie-App-React-WebApp/movie-webapp/src/pages/UserProfile/UserProfile.jsx
+++ Movie-App-React-WebApp/movie-webapp/src/pages/UserProfile/UserProfile.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useAuth } from '../../Context/AuthContext';
+import genericAvatar from '../../assets/user.jpg'; // Asegúrate de que la ruta sea correcta
 
 const UserProfile = () => {
   const { authUser } = useAuth();
@@ -15,22 +16,18 @@ const UserProfile = () => {
         <div className="max-w-md mx-auto bg-gray-800 rounded-lg shadow-lg overflow-hidden">
           <div className="px-6 py-8">
             {/* Avatar */}
-            <div className="flex justify-center">
+            <div className="flex justify-center mb-4">
               <img
-                src={user.avatarUrl || 'https://via.placeholder.com/150'}
+                src={user.avatarUrl || genericAvatar}
                 alt="Avatar"
                 className="h-40 w-40 rounded-full object-cover border-4 border-netflix-red"
               />
             </div>
             {/* Información del perfil */}
-            <div className="text-center mt-6">
-              <h2 className="text-3xl font-bold">{user.firstName} {user.lastName}</h2>
-              <p className="text-sm text-gray-400">username: @{user.username}</p>
-              <p className="text-sm text-gray-400">email: {user.email}</p>
-              <p className="text-sm text-gray-300 mt-4">{user.bio}</p>
-              <p className="text-sm text-gray-300 mt-4">
-                <strong>Location:</strong> {user.location}
-              </p>
+            <div className="text-center">
+              <h2 className="text-3xl font-bold mb-2">{user.firstName} {user.lastName}</h2>
+              <p className="text-sm text-gray-400 mb-2">username: @{user.username}</p>
+              <p className="text-sm text-gray-400 mb-2">email: {user.email}</p>
             </div>
           </div>
         </div>
